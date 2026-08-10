37e525a85d6be93b942f3eaa26d66ee3cfad9296
Luca Cozzuto <lucacozzuto@gmail.com>
2026-08-10 12:21:41 +0200

    Fix GAPI VCF module class casing (formats.VCF)

--- functions.py
+++ functions.py
@@ -123,7 +123,7 @@ def filter_sd(dict_mutations, key_list):
     return dict_mutations
 
 def read_vcf_file_BED(file_path, sv_type='insertion'):
-    vcf = formats.vcf.vcf_parser(file_path)
+    vcf = formats.VCF.vcf_parser(file_path)
     data = []
     type_key = 'ITYPE_N' if sv_type == 'insertion' else 'DTYPE_N'
     for variant in vcf.variants:
